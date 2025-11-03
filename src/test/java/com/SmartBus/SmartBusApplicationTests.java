55523cde635ae7ff56a8de8833be0defbd5f163b
camdao <luongcamdao@gmail.com>
2025-11-03 22:39:21 +0700

    init project

---
+++ src/test/java/com/SmartBus/SmartBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
